38c1fccf8216c90e06b9c9672e43c24c92198699
Matthew <10925568@my.uvu.edu>
2020-10-02 13:51:35 -0600

    Controller script

---
+++ 1610Fall2020 ass0/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public Vector3 moveDirection;
+
+    private void Update()
+    {
+        var movement = moveDirection * Time.deltaTime;
+        controller.Move(movement);
+    }
+}
