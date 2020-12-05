7d98e05dc0edbaf009f6e113196cf51175e23142
Matthew <10925568@my.uvu.edu>
2020-12-04 17:27:27 -0700

    Old shit i need to catch up on

---
+++ 1610Fall2020 ass0/Assets/Scripts/ApplyForces.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ApplyForces : MonoBehaviour
+{
+
+    public Rigidbody rBody;
+    public float force = 100;
+    
+    void OnMouseDown()
+    {
+        rBody.AddForce(0, force,0);
+    }
+    
+    void Update()
+    {
+        
+    }
+}
--- 1610Fall2020 ass0/Assets/Scripts/CharacterMover.cs
+++ 1610Fall2020 ass0/Assets/Scripts/CharacterMover.cs
@@ -6,11 +6,27 @@ using UnityEngine;
 public class CharacterMover : MonoBehaviour
 {
     public CharacterController controller;
+    public float moveSpeed = 5f, gravity = -9.81f, jumpForce = 5f;
 
     public Vector3 moveDirection;
-
+    private float yDirection;
     private void Update()
     {
+        var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal");
+        moveDirection.Set(moveSpeedInput, yDirection, 0);
+
+        yDirection += gravity * Time.deltaTime;
+
+        if (controller.isGrounded && moveDirection.y < 0)
+        {
+            yDirection = -1f;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            yDirection = jumpForce;
+        }
+
         var movement = moveDirection * Time.deltaTime;
         controller.Move(movement);
     }
