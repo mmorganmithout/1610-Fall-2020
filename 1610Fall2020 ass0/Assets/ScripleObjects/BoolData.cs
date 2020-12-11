b3291a5e1f6b153ddd8edaaf00f053dfbb1b6f7f
Matthew <10925568@my.uvu.edu>
2020-12-11 12:31:33 -0700

    Dialogue List

---
+++ 1610Fall2020 ass0/Assets/ScripleObjects/BoolData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class BoolData : ScriptableObject
+{
+    public bool value;
+
+    public void UpdateValue(bool newValue)
+    {
+        value = newValue;
+        Debug.Log("Game Over");
+    }
+}
