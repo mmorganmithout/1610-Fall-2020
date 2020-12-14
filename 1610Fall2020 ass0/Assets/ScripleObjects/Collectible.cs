d227651d85d98d3d7401db64cd673de378661e9e
Matthew <10925568@my.uvu.edu>
2020-12-14 13:12:14 -0700

    Loops and Collectibles

---
+++ 1610Fall2020 ass0/Assets/ScripleObjects/Collectible.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Collectible : ScriptableObject
+{
+    public float powerLevel = 1f;
+    public int cashValue = 10;
+    public Sprite art;
+    public Color artColor = Color.blue;
+}
