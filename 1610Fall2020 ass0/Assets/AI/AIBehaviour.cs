a387523c8cc319bd62304d81f1dee05c165fb51a
Matthew <10925568@my.uvu.edu>
2020-12-15 14:39:23 -0700

    More AI stuff

--- 1610Fall2020 ass0/Assets/AI/AIBehaviour.cs
+++ 1610Fall2020 ass0/Assets/AI/AIBehaviour.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,15 +6,33 @@ using UnityEngine.AI;
 public class AIBehaviour : MonoBehaviour
 {
     private NavMeshAgent agent;
-    public Transform player;
-    
+    private Transform destination;
+    private bool canPatrol = true;
+    private int i = 0;
+
+    public List<Transform> patrolPoints;
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        destination = transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canPatrol = false;
+        destination = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canPatrol = true;
     }
 
     private void Update()
     {
-        agent.destination = player.position;
+        agent.destination = destination.position;
+        if (!canPatrol) return;
+
+        destination = patrolPoints[i];
     }
 }
