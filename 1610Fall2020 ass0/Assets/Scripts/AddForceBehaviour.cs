b8e32b6d10534c765642864f8849c751512b39c3
Matthew <10925568@my.uvu.edu>
2020-12-15 15:35:22 -0700

    While loops and corountines

---
+++ 1610Fall2020 ass0/Assets/Scripts/AddForceBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AddForceBehaviour : MonoBehaviour
+{
+    private Rigidbody rigid;
+    public Vector3 forces, forces2;
+    public float holdTime = 3f;
+    public int counter = 10;
+    public bool canRun;
+    
+    private IEnumerator Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(holdTime);
+            rigid.AddForce(forces);
+            counter--;
+        }
+        
+        while (canRun)
+        {
+            yield return new WaitForSeconds(holdTime);
+            rigid.AddForce(forces2);
+        }
+
+    }
+}
---
+++ 1610Fall2020 ass0/Assets/Scripts/CoroutineBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehaviour : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float holdTime, repeatHoldTime = 0.25f;
+    public int counter = 3;
+
+    public void RunCoroutine()
+    {
+        StartCoroutine(Corountine());
+    }
+
+    private IEnumerator Corountine()
+    {
+        startEvent.Invoke();
+        
+        yield return new WaitForSeconds(holdTime);
+
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return new WaitForSeconds(repeatHoldTime);
+            counter--;
+        }
+        
+        yield return new WaitForSeconds(holdTime);
+        endEvent.Invoke();
+    }
+}
